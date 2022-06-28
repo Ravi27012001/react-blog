68ee456c1ea68117e1a366ba66f5f06caf942827
Ravikant <raviidhoni@gmail.com>
2022-06-28 17:29:25 +0530

    done

---
+++ src/Component/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    <div className='container__header'>
+                <p className='the'>The</p>
+                <h1 className='siren' >Siren</h1>
+                
+    </div>
+    </div>
+  )
+}
+
+export default Header
